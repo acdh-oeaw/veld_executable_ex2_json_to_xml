40b21c405f147356ded94fc01997765ea807e0b8
steffres <stefan.resch@oeaw.ac.at>
2023-07-03 23:03:09 +0200

    fixed bug in list of list handling

--- app.py
+++ app.py
@@ -10,15 +10,14 @@ file_path_output = "/veld/output/data.xml"
 def dict_to_xml(d, node_current=None):
     if node_current is None:
         node_current = ET.Element("root")
-    for k,v in d.items():
-        node_k = ET.Element(k)
-        if type(v) is dict:
+    k_v_pairs = None
+    if type(d) is dict:
+        k_v_pairs = [(ET.Element(k), v) for k, v in d.items()]
+    elif type(d) is list:
+        k_v_pairs = [(ET.Element("li"), v) for v in d]
+    for node_k, v in k_v_pairs:
+        if type(v) is dict or type(v) is list:
             dict_to_xml(v, node_k)
-        elif type(v) is list:
-            for lv in v:
-                li = ET.Element("li")
-                li.text = lv
-                node_k.append(li)
         else:
             node_k.text = str(v)
         node_current.append(node_k)
